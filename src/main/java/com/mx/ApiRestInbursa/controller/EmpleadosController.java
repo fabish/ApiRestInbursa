4a1757245818d048542b64e845630ecccfa26fc4
fabish <123667845+fabish@users.noreply.github.com>
2026-07-13 09:23:35 -0600

    Se agrago libreria

--- src/main/java/com/mx/ApiRestInbursa/controller/EmpleadosController.java
+++ src/main/java/com/mx/ApiRestInbursa/controller/EmpleadosController.java
@@ -1,5 +1,7 @@
 package com.mx.ApiRestInbursa.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
